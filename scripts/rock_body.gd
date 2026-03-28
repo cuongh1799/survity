597816e04d9edbd2e843dcf2e5d8e41570208651
cuongh1799 <cuonghuynh1009@gmail.com>
2026-03-28 12:20:42 +0700

    drag now highlights yellow

--- scripts/rock_body.gd
+++ scripts/rock_body.gd
@@ -2,24 +2,16 @@ extends StaticBody3D
 
 @onready var mesh = $RockShape/stone_tallA2 # Adjust this to match your child mesh name
 
-#func set_highlight(active: bool):
-	## This looks for the first MeshInstance3D inside the object
-	#var mesh_node = find_child("*", true, false)
-#
-	## We check if it exists and is actually a MeshInstance3D
-	#if mesh_node and mesh_node is MeshInstance3D:
-		#mesh_node.transparency = 0.5 if active else 0.0
-	#else:
-		## If it's a Node3D 'wrapper', search one level deeper
-		#for child in get_children():
-			#var deep_mesh = child.find_child("*", true, false)
-			#if deep_mesh is MeshInstance3D:
-				#deep_mesh.transparency = 0.5 if active else 0.0
-				#break
+@onready var selection_visual = $SelectionVisual
+
+func _ready():
+	# Make sure we are in the group for the camera script to find us
+	add_to_group("props")
+	# Start hidden
+	if selection_visual:
+		selection_visual.visible = false
+
+func set_highlight(active: bool):
+	if selection_visual:
+		selection_visual.visible = active
 ## Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
