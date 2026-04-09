437724be4fd0056811875cbb6e9efd24a8ae3fd6
Nicle <s4138091@rmit.edu.vn>
2026-04-09 17:18:28 +0700

    Create main menu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,36 @@
+extends Control
+@onready var main_button: VBoxContainer = $Main_button
+@onready var option_setting: Panel = $Option_Setting
+@onready var title: Label = $Title
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	option_setting.visible = false
+	main_button.visible = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().scene_changed
+	option_setting.visible = false
+	main_button.visible = false
+	title.visible = false
+
+func _on_option_button_pressed() -> void:
+	option_setting.visible = true
+	main_button.visible = false
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_buton_pressed() -> void:
+	option_setting.visible = false
+	main_button.visible = true
