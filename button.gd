c10c010cdd18428672d8bf39792172407c7d7cb8
cuongh1799 <cuonghuynh1009@gmail.com>
2026-04-10 14:41:28 +0700

    added some settyings

--- button.gd
+++ button.gd
@@ -3,11 +3,11 @@ extends Button
 var item_scene: PackedScene
 
 func setup(scene: PackedScene):
-    item_scene = scene
-    # Set the button text to the filename of the scene (e.g., "House")
-    text = scene.resource_path.get_file().get_basename()
+	item_scene = scene
+	# Set the button text to the filename of the scene (e.g., "House")
+	text = scene.resource_path.get_file().get_basename()
 
 func _on_pressed():
-    # Signal your PropSpawner or Building Manager to start placing this item
-    print("Selected to build: ", item_scene.resource_path)
-    # GlobalSignal.emit_signal("item_selected", item_scene)
+	# Signal your PropSpawner or Building Manager to start placing this item
+	print("Selected to build: ", item_scene.resource_path)
+	# GlobalSignal.emit_signal("item_selected", item_scene)
