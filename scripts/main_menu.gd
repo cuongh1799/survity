b1758451b502d0c44eb44471f445a466a7a249e4
Nicle <s4138091@rmit.edu.vn>
2026-05-08 16:32:10 +0700

    add main menu in game play

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,31 +4,51 @@ extends Control
 @onready var game_manual: Panel = $GameManual
 @onready var title: Label = $Title
 
+@onready var budget_ui: Control = $"../BudgetUI"
+@onready var mission_ui: Control = $"../MissionUI"
+@onready var shop_ui: Control = $"../ShopUI"
+@onready var inventory: Control = $"../Inventory"
+@onready var coords_label: Label = $"../CoordsLabel"
+@onready var top_view_button: Button = $"../TopViewButton"
+@onready var harvest_button: Button = $"../HarvestButton"
+@onready var interaction_prompt: Control = $"../InteractionPrompt"
+@onready var minimap_ui: Control = $"../MinimapUI"
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	option_setting.visible = false
-	main_button.visible = true
 	game_manual.visible = false
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	main_button.visible = true
+	title.visible = true
+	
+	budget_ui.visible = false
+	mission_ui.visible = false
+	shop_ui.visible = false
+	inventory.visible = false
+	coords_label.visible = false
+	top_view_button.visible = false
+	interaction_prompt.visible = false
+	minimap_ui.visible = false
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().scene_changed
-	option_setting.visible = false
 	main_button.visible = false
 	title.visible = false
-	game_manual.visible = false
+	
+	budget_ui.visible = true
+	mission_ui.visible = true
+	shop_ui.visible = true
+	inventory.visible = true
+	coords_label.visible = true
+	top_view_button.visible = true
+	interaction_prompt.visible = true
+	minimap_ui.visible = true
 
 func _on_option_button_pressed() -> void:
 	option_setting.visible = true
 	main_button.visible = false
-	game_manual.visible = false
 
 
 func _on_quit_button_pressed() -> void:
@@ -42,10 +62,8 @@ func _on_back_buton_pressed() -> void:
 
 func _on_manual_pressed() -> void:
 	game_manual.visible = true
-	option_setting.visible = false
 	main_button.visible = false
 
 func _on_back_button_manual_pressed() -> void:
-	option_setting.visible = false
 	main_button.visible = true
 	game_manual.visible = false
